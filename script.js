5e45ebdc2f1dd513003e07596904ee5fc740c2ff
kali-tan <tank4@vcu.edu>
2025-02-11 15:19:16 -0500

    transfer content

---
+++ script.js
@@ -0,0 +1,89 @@
+$(".chimchar").click(
+    function(){
+        window.open("files/resume.pdf");
+    }
+);
+
+$(".squirtle").click(
+    function(){
+        window.open("https://www.linkedin.com/in/kalissa-tan-statistics");
+    }
+);
+
+$(".bulbasaur").click(
+    function(){
+        window.open("mailto:tan.kalissa@gmail.com");
+    }
+);
+
+$(".logo").click(
+    function(){
+        window.open("index.html");
+    }
+);
+
+$(".about").click(
+    function(){
+        window.open("about.html");
+    }
+);
+
+$(".design").click(
+    function(){
+        window.open("design.html");
+    }
+);
+
+$(".goodroom").click(
+    function(){
+        window.open("goodroom.html");
+    }
+);
+
+$(".recruitment").click(
+    function(){
+        window.open("recruitment.html");
+    }
+);
+
+$(".schange").click(
+    function(){
+        window.open("schange.html");
+    }
+);
+
+$(".dp").click(
+    function(){
+        window.open("dp.html");
+    }
+);
+
+$(".justjewelry").click(
+    function(){
+        window.open("justjewelry.html");
+    }
+);
+
+$(".pigment").click(
+    function(){
+        window.open("pigment.html");
+    }
+);
+
+$(".strlit").click(
+    function(){
+        window.open("strlit.html");
+    }
+);
+
+$(".eco").click(
+    function(){
+        window.open("eco.html");
+    }
+);
+
+
+
+
+
+
